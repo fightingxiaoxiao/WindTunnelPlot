64ac22f174902554a698a3d78cb63e587396fd1e
Xiaoxiao <smilemax@foxmail.com>
2021-09-28 16:57:20 +0800

    提供合并Excel表函数 & 上传二进制文件及数据

--- extactPoint.py
+++ extactPoint.py
@@ -27,27 +27,48 @@ def readPoints(filename):
     return points
 
 
-def addToExcel(filename, points):
+def mergeExcel(filenames):
+    """
+    合并多个标准测压数据文件.
+    """
+    dfs = []
+
+    for f in filenames:
+        dfs.append(pd.read_excel(f, sheet_name=None, index_col=0))
+
+    fileNum = len(filenames)
+    for key in dfs[0]:
+        for i in range(fileNum - 1):
+            dfs[i + 1][key] = pd.concat([dfs[i][key], dfs[i + 1][key]])
+    print(dfs[-1]['0'])
+
+def addToExcel(filename, points, newFilename):
     df_t = pd.read_excel(filename, sheet_name=None, index_col=0)
+    writer = pd.ExcelWriter(newFilename)
     for key in df_t:
         df = df_t[key]
-        df.insert(0, 'x', 10000)
-        df.insert(1, 'y', 10000)
-        df.insert(2, 'z', 10000)
+        print(key)
+        print(df)
+        df.insert(0, 'x', 1E32)
+        df.insert(1, 'y', 1E32)
+        df.insert(2, 'z', 1E32)
 
-    for p_name in points:
-        df.loc[p_name,'x'] = points[p_name][0]
-        df.loc[p_name,'y'] = points[p_name][1]
-        df.loc[p_name,'z'] = points[p_name][2]
-        #print(df.loc[p_name])
-    
-    df.to_excel('test.xlsx',sheet_name='0',index='name')
+        for p_name in points:
+            df.loc[p_name, 'x'] = points[p_name][0]
+            df.loc[p_name, 'y'] = points[p_name][1]
+            df.loc[p_name, 'z'] = points[p_name][2]
+
+        df.to_excel(writer, sheet_name=key)
+    writer.close()
+
 
 def main():
-    filename = 'TEST01.obj'
-    excelName = 'resultA851.xlsx'
+    filename = 'points.obj'
+    excelName = 'all243.xlsx'
+    newExcelName = 'result.xlsx'
+    #mergeExcel(['resultA851.xlsx'])
     points = readPoints(filename)
-    addToExcel(excelName, points)
+    addToExcel(excelName, points, newExcelName)
 
 
 if __name__ == '__main__':
